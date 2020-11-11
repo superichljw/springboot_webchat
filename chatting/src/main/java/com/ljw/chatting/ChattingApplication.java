04d201be3a809000fc57884d0080207ba7045113
superichljw <60783689+superichljw@users.noreply.github.com>
2020-11-11 21:32:27 +0900

    Add files via upload

---
+++ chatting/src/main/java/com/ljw/chatting/ChattingApplication.java
@@ -0,0 +1,14 @@
+package com.ljw.chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingApplication.class, args);
+	}
+
+}
---
+++ chatting/src/test/java/com/ljw/chatting/ChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljw.chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
